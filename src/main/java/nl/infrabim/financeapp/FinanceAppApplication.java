6b8ef28f6c7d5ad1a7273ae6e6f87fc5b4f4d5dd
Peter Willems <peter.willems@infrabim.nl>
2023-05-04 14:22:30 +0200

    Improved csv file importing procedure

--- src/main/java/nl/infrabim/financeapp/FinanceAppApplication.java
+++ src/main/java/nl/infrabim/financeapp/FinanceAppApplication.java
@@ -4,8 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class FinanceAppApplication {
@@ -13,18 +19,23 @@ public class FinanceAppApplication {
     public static List<File> csvFiles = null;
 
     public static void main(String[] args) {
-        if (args.length > 0) {
-            csvFiles = new ArrayList<>();
-            try {
+        csvFiles = new ArrayList<>();
+        try {
+            if (args.length > 0) {
                 for (int i = 0; i < args.length; i++) {
                     File csvFile = new File(args[i]);
                     csvFiles.add(csvFile);
                 }
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
+            } else {
+                Path start = new File(System.getProperty("user.dir")).toPath();
+                Files.list(start)
+                        .filter((p) -> p.toString().endsWith(".csv"))
+                        .forEach((p) -> csvFiles.add(new File(p.toString())));
             }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
         }
         SpringApplication.run(FinanceAppApplication.class, args);
     }
-
 }
+
